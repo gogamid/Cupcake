104d3e516ba747194c58aea42cf602668bd6cc80
Imron <imron.gamidli@lt.hs-fulda.de>
2022-05-24 14:09:25 +0200

    feat: implement navigation to summary fragment

--- app/src/main/java/com/example/cupcake/fragments/PickupDateFragment.kt
+++ app/src/main/java/com/example/cupcake/fragments/PickupDateFragment.kt
@@ -5,6 +5,8 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import androidx.navigation.fragment.findNavController
 import com.example.cupcake.R
 
 class PickupDateFragment : Fragment() {
@@ -15,5 +17,13 @@ class PickupDateFragment : Fragment() {
   ): View? {
     // Inflate the layout for this fragment
     return inflater.inflate(R.layout.fragment_pickup_date, container, false)
+  }
+
+  override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+    view.findViewById<Button>(R.id.next_button).setOnClickListener { goToNextScreen() }
+  }
+
+  private fun goToNextScreen() {
+    findNavController().navigate(R.id.action_pickupDateFragment_to_summaryFragment)
   }
 }
