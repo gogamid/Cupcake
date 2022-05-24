33f00c3dd53889cc68d8732baf121571fc20f420
Imron <imron.gamidli@lt.hs-fulda.de>
2022-05-24 13:59:48 +0200

    feat: use navigation graph

--- app/src/main/java/com/example/cupcake/fragments/MainFragment.kt
+++ app/src/main/java/com/example/cupcake/fragments/MainFragment.kt
@@ -1,9 +1,11 @@
 package com.example.cupcake.fragments
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import androidx.fragment.app.Fragment
 import com.example.cupcake.R
 
@@ -14,5 +16,15 @@ class MainFragment : Fragment() {
   ): View? {
     // Inflate the layout for this fragment
     return inflater.inflate(R.layout.fragment_main, container, false)
+  }
+
+  override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+    view.findViewById<Button>(R.id.one_cupcake).setOnClickListener { orderCupcake(1) }
+    view.findViewById<Button>(R.id.six_cupcakes).setOnClickListener { orderCupcake(6) }
+    view.findViewById<Button>(R.id.twelve_cupcakes).setOnClickListener { orderCupcake(12) }
+  }
+
+  private fun orderCupcake(quantity: Int) {
+    Log.d("test", "Quantity $quantity")
   }
 }
