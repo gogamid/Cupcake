f103296a3591c237d7f1f499b06076a24b5988e9
Imron <imron.gamidli@lt.hs-fulda.de>
2022-06-04 10:15:31 +0200

    feat: use binding expressions for radio buttons for pickup date

--- app/src/main/java/com/example/cupcake/fragments/PickupDateFragment.kt
+++ app/src/main/java/com/example/cupcake/fragments/PickupDateFragment.kt
@@ -6,20 +6,30 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
+import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.activityViewModels
 import androidx.navigation.fragment.findNavController
 import com.example.cupcake.R
+import com.example.cupcake.databinding.FragmentFlavorBinding
+import com.example.cupcake.databinding.FragmentPickupDateBinding
+import com.example.cupcake.model.OrderViewModel
 
 class PickupDateFragment : Fragment() {
+
+  private val sharedViewModel: OrderViewModel by activityViewModels()
+  private lateinit var binding: FragmentPickupDateBinding
 
   override fun onCreateView(
     inflater: LayoutInflater, container: ViewGroup?,
     savedInstanceState: Bundle?
-  ): View? {
-    // Inflate the layout for this fragment
-    return inflater.inflate(R.layout.fragment_pickup_date, container, false)
+  ): View {
+    binding = DataBindingUtil.inflate(inflater, R.layout.fragment_pickup_date, container, false)
+    return binding.root
   }
 
   override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+    binding.viewModel = sharedViewModel
+    binding.lifecycleOwner = viewLifecycleOwner
     view.findViewById<Button>(R.id.next_button).setOnClickListener { goToNextScreen() }
   }
 
--- app/src/main/java/com/example/cupcake/model/OrderViewModel.kt
+++ app/src/main/java/com/example/cupcake/model/OrderViewModel.kt
@@ -4,19 +4,23 @@ import android.util.Log
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import java.text.SimpleDateFormat
+import java.util.*
 
 class OrderViewModel : ViewModel() {
-  private val _quantity = MutableLiveData(0)
+  private val _quantity = MutableLiveData<Int>()
   val quantity: LiveData<Int> = _quantity
 
-  private val _flavor = MutableLiveData("")
+  private val _flavor = MutableLiveData<String>()
   val flavor: LiveData<String> = _flavor
 
-  private val _date = MutableLiveData("")
+  private val _date = MutableLiveData<String>()
   val date: LiveData<String> = _date
 
-  private val _price = MutableLiveData(0.0)
+  private val _price = MutableLiveData<Double>()
   val price: LiveData<Double> = _price
+
+  val dateOptions = getPickupOptions()
 
   fun setQuantity(quantity: Int) {
     _quantity.value = quantity
@@ -24,7 +28,6 @@ class OrderViewModel : ViewModel() {
 
   fun setFlavor(flavor: String) {
     _flavor.value = flavor
-    Log.d("test", "Flavor has been set to ${_flavor.value}")
   }
 
   fun setDate(date: String) {
@@ -33,5 +36,27 @@ class OrderViewModel : ViewModel() {
 
   fun hasNoFlavorSet(): Boolean {
     return _flavor.value.isNullOrEmpty()
+  }
+
+  private fun getPickupOptions(): List<String> {
+    val options = mutableListOf<String>()
+    val formatter = SimpleDateFormat("E MMM d", Locale.getDefault())
+    val calendar = Calendar.getInstance()
+    repeat(4) {
+      options.add(formatter.format(calendar.time))
+      calendar.add(Calendar.DATE, 1)
+    }
+    return options
+  }
+
+  fun resetOrder() {
+    _quantity.value = 0
+    _flavor.value = ""
+    _date.value = dateOptions[0]
+    _price.value = 0.0
+  }
+
+  init {
+    resetOrder()
   }
 }
