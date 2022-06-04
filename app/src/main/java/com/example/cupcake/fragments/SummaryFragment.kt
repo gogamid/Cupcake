daf3229e1b5434dbf2d3547f733cda1db79d1fa1
Imron <imron.gamidli@lt.hs-fulda.de>
2022-06-04 10:26:53 +0200

    feat: use binding expressions for summary fragment

--- app/src/main/java/com/example/cupcake/fragments/SummaryFragment.kt
+++ app/src/main/java/com/example/cupcake/fragments/SummaryFragment.kt
@@ -5,16 +5,31 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.activityViewModels
 import com.example.cupcake.R
+import com.example.cupcake.databinding.FragmentSummaryBinding
+import com.example.cupcake.model.OrderViewModel
 
 
 class SummaryFragment : Fragment() {
 
+  private val sharedViewModel: OrderViewModel by activityViewModels()
+  private lateinit var binding: FragmentSummaryBinding
+
   override fun onCreateView(
     inflater: LayoutInflater, container: ViewGroup?,
     savedInstanceState: Bundle?
-  ): View? {
+  ): View {
     // Inflate the layout for this fragment
-    return inflater.inflate(R.layout.fragment_summary, container, false)
+    binding = DataBindingUtil.inflate(inflater, R.layout.fragment_summary, container, false)
+    return binding.root
+  }
+
+
+  override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+    super.onViewCreated(view, savedInstanceState)
+    binding.viewModel = sharedViewModel
+    binding.lifecycleOwner = viewLifecycleOwner
   }
 }
